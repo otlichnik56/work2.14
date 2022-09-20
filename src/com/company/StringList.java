6015c641949118de98fd3aa038a0025d00c2b0b0
otlichnik56 <otlichnik56@yandex.ru>
2022-09-20 10:29:21 +0500

    Домашка 2.15 Делал на работе

--- src/com/company/StringList.java
+++ src/com/company/StringList.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class StringList {
 
-    private final List<String> result = new ArrayList<>();
+    private final List<Integer> result = new ArrayList<>();
 
     // Добавление элемента.
     // Вернуть добавленный элемент в качестве результата выполнения.
-    public String addStringAddReturnResult(String item) {
+    public Integer addStringAddReturnResult(Integer item) {
         result.add(item);
         return item;
     }
@@ -17,7 +17,7 @@ public class StringList {
     // Добавление элемента на определенную позицию списка.
     // Если выходит за пределы фактического количества элементов или массива, выбросить исключение.
     // Вернуть добавленный элемент в качестве результата выполнения.
-    public String addStringOfPozitionAddReturnResult(int index, String item) {
+    public Integer addStringOfPozitionAddReturnResult(int index, Integer item) {
         if (index > result.size()){
             throw new RuntimeException("выходит за пределы фактического количества элементов");
         }
@@ -27,7 +27,7 @@ public class StringList {
 
     // Установить элемент на определенную позицию, затерев существующий.
     // Выбросить исключение, если индекс больше фактического количества элементов или выходит за пределы массива.
-    public void setStringOfPozition(int index, String item) {
+    public void setStringOfPozition(int index, Integer item) {
         if (index >= result.size()){
             throw new RuntimeException("выходит за пределы фактического количества элементов");
         }
@@ -36,7 +36,7 @@ public class StringList {
 
     // Удаление элемента.
     // Вернуть удаленный элемент или исключение, если подобный элемент отсутствует в списке.
-    public String removeStringOnValue(String item) {
+    public Integer removeStringOnValue(Integer item) {
         if (!result.contains(item)){
             throw new RuntimeException("подобный элемент отсутствует в списке");
         }
@@ -46,23 +46,23 @@ public class StringList {
 
     // Удаление элемента по индексу.
     // Вернуть удаленный элемент или исключение, если подобный элемент отсутствует в списке.
-    public String removeStringOnIndex(int index) {
+    public Integer removeStringOnIndex(int index) {
         if (index >= result.size()){
             throw new RuntimeException("подобный элемент отсутствует в списке");
         }
-        String item = result.get(index);
+        Integer item = result.get(index);
         result.remove(index);
         return item;
     }
 
     // Проверка на существование элемента. Вернуть true/false
-    public boolean checkingExistenceOfElement(String item) {
+    public boolean checkingExistenceOfElement(Integer item) {
         return result.contains(item);
     }
 
     // Поиск элемента.
     // Вернуть индекс элемента или -1 в случае отсутствия.
-    public int findIndexElement(String item) {
+    public int findIndexElement(Integer item) {
         if (checkingExistenceOfElement(item)){
             return result.indexOf(item);
         } else {
@@ -72,7 +72,7 @@ public class StringList {
 
     // Поиск элемента с конца.
     // Вернуть индекс элемента или -1 в случае отсутствия.
-    public int findIndexElementOnEnd(String item) {
+    public int findIndexElementOnEnd(Integer item) {
         if (checkingExistenceOfElement(item)){
             return result.lastIndexOf(item);
         } else {
@@ -83,7 +83,7 @@ public class StringList {
 
     // Получить элемент по индексу.
     // Вернуть элемент или исключение, если выходит за рамки фактического количества элементов.
-    public String getStringOnIndex(int index) {
+    public Integer getStringOnIndex(int index) {
         if (index >= result.size()){
             throw new RuntimeException("выходит за рамки фактического количества элементов");
         }
@@ -92,7 +92,7 @@ public class StringList {
 
     // Сравнить текущий список с другим.
     // Вернуть true/false или исключение, если передан null.
-    public boolean equalsStringList(ArrayList<String> otherList) {
+    public boolean equalsStringList(ArrayList<Integer> otherList) {
         if (otherList == null){
             throw new RuntimeException("передан null");
         } else {
@@ -105,7 +105,6 @@ public class StringList {
         return result.size();
     }
 
-
     // Вернуть true, если элементов в списке нет, иначе false.
     public boolean sizeIsEmpty() {
         return result.isEmpty();
@@ -117,8 +116,74 @@ public class StringList {
     }
 
     // Создать новый массив из строк в списке и вернуть его.
-    public List<String> createNewStringList() {
+    public List<Integer> createNewStringList() {
         return new ArrayList<>(result);
+    }
+
+    // Пузырьковая сортировка. Сортирует где-то 39-40 секунд
+    public static void sortBubble(List<Integer> arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            for (int j = 0; j < arr.size() - 1 - i; j++) {
+                if (arr.get(j) > arr.get(j + 1)) {
+                    Integer m = arr.get(j);
+                    Integer n = arr.get(j + 1);
+                    arr.set(j, n);
+                    arr.set(j + 1, m);
+                }
+            }
+        }
+    }
+
+    // Сортировка выбором. Сортирует где-то 11-12 секунд
+    public static void sortSelection(List<Integer> arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j) < arr.get(minElementIndex)) {
+                    minElementIndex = j;
+                }
+            }
+            Integer m = arr.get(minElementIndex);
+            Integer n = arr.get(i);
+            arr.set(minElementIndex, n);
+            arr.set(i, m);
+        }
+    }
+
+    // Сортировка вставкой. Сортирует где-то 10-11 секунд
+    public static void sortInsertion(List<Integer> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            int temp = arr.get(i);
+            int j = i;
+            while (j > 0 && arr.get(j - 1) >= temp) {
+                  Integer n = arr.get(j - 1);
+                arr.set(j, n);
+                j --;
+            }
+            arr.set(j, temp);
+        }
+    }
+
+    // Бинарный поиск
+    private static boolean contains(List<Integer> arr, int element) {
+        sortInsertion(arr);
+        int min = 0;
+        int max = arr.size() - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (element == arr.get(mid)) {
+                return true;
+            }
+
+            if (element < arr.get(mid)) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/TestString.java
+++ src/test/TestString.java
@@ -1,6 +1,54 @@
 package test;
 
+import com.company.StringList;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestString {
 
+    private List<Integer> list1;
+    private List<Integer> list2;
+    private List<Integer> list3;
+
+    @BeforeEach
+    public void initOut() {
+        Integer[] array = new Integer[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random() * 100000));
+        }
+
+        list1 = new ArrayList<>();
+        list2 = new ArrayList<>();
+        list3 = new ArrayList<>();
+
+        list1 = Arrays.asList(array);
+        list2 = Arrays.asList(array);
+        list3 = Arrays.asList(array);
+    }
+
+    @Test
+    public void testSortBubble() {
+        long start = System.currentTimeMillis();
+        StringList.sortBubble(list1);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    @Test
+    public void testSortSelection() {
+        long start = System.currentTimeMillis();
+        StringList.sortSelection(list2);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    @Test
+    public void testSortInsertion() {
+        long start = System.currentTimeMillis();
+        StringList.sortInsertion(list3);
+        System.out.println(System.currentTimeMillis() - start);
+    }
 
 }
